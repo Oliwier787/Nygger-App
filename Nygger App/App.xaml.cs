a71824dea3b3a4a83a5102417896fc1caed97058
1680183 <1680183@TE-GDA-D-D21E18>
2024-09-16 08:41:06 +0200

    Dodaj pliki projektów.

---
+++ Nygger App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Nygger_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
